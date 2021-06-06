ba76bb7e2e74872f21f2fad18df667f13c0cdf77
cristianvelascofm <cfvelasco@unicauca.edu.co>
2021-06-06 17:56:09 -0500

    OROS Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    
+    <v-main id="aplication" class="ma-0 pa-0">
+      <v-container fluid class="ma-0 pa-0 ba-0">
+        <v-layout
+          
+          class=""
+          fill-height
+          style="height: 713px"
+          justify-center
+          align-center
+        >
+          <v-flex id="left_panel" class="white--text text-center" xs5>
+            <v-layout
+              class="text-center"
+              justify-center
+              align-center
+              fill-height
+              style="height: 570px"
+            >
+              <v-flex class="text-center" xs5>
+                <v-img
+                  class="shrink"
+                  flex
+                  src="./assets/img/oros_logo.gif"
+                  min-height="200"
+                  height="200"
+                  min-width="200"
+                  width="200"
+                ></v-img>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex id="right_panel" class="pink--text text-center pa-15" xs5>
+            <router-view/>      
+          </v-flex>
+        </v-layout>
+      </v-container>
+      
+    </v-main>
+    <v-footer id='pie' xs-10 class="justify-center"  padless>
+       <v-card-text class="dark--text text-center">
+         <strong > Universidad del Cauca | Vicerrectoría Administrativa | SOLEMDev</strong> — {{ new Date().getFullYear() }} 
+      </v-card-text>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
+  name: "App",
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  data: () => ({
+    //
+  }),
+};
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+#aplication , #pie{
+  background: linear-gradient(
+    135deg,
+    rgb(230, 227, 220) 0%,
+    rgb(238, 217, 214) 100%
+  );
 }
 </style>
